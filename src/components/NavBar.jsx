e289a622a1afccff07b19b6d92adc92f0e3f3dbc
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2023-05-12 20:43:25 -0300

    cambio en titulo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -72,7 +72,7 @@ function NavBar() {
                 color: "inherit",
               }}
             >
-              CalzaTodo
+              CalzaME
             </Typography>
           </Link>
 
